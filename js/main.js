22e62791729f9dd8847bc4ca4db0bdcbc0c273b6
ruhheeb <ruhheebmusic@gmail.com>
2020-08-11 14:48:09 +0400

    create: added section about

--- js/main.js
+++ js/main.js
@@ -9,4 +9,38 @@ $(document).ready(function () {
     $(activeContent).addClass("trend-tabs__content--active");
     $(this).addClass("trend-tabs__item--active");
   });
+
+
+  var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    // direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      // renderBullet: function (index, className) {
+      //   return `<span class="dot swiper-pagination-bullet">${index}</span>`;
+      // },
+    },
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+    mousewheel: {
+      invert: true,
+    },
+  })
+
 });
